7cf9fa24a339df0754ad3ce990d967ced13c2b2f
bhatiakanishk <bhatia.ka@northeastern.edu>
2023-04-04 13:02:51 -0400

    autoscaling

--- ec2.tf
+++ ec2.tf
@@ -11,48 +11,48 @@ variable "sql_password" {
   default = "Kanu1327"
 }
 
-resource "aws_instance" "my_ec2_instance" {
-  count                       = 1
-  ami                         = var.ami_id
-  instance_type               = "t2.micro"
-  vpc_security_group_ids      = [aws_security_group.app_security_group.id]
-  subnet_id                   = aws_subnet.public_subnet[0].id
-  associate_public_ip_address = true
-  iam_instance_profile        = aws_iam_instance_profile.instance_profile.name
-  key_name                    = "ec2"
-  user_data                   = <<EOF
-#!/bin/bash
-echo "SQLUSER="${var.sql_user}"" >> /home/ec2-user/webapp/.env
-echo "SQLPASSWORD="${var.sql_password}"" >> /home/ec2-user/webapp/.env
-echo "DATABASENAME=csye6225" >> /home/ec2-user/webapp/.env
-echo "SQLHOSTNAME="${aws_db_instance.rds_instance.address}"" >> /home/ec2-user/webapp/.env
-echo "BUCKETNAME="${aws_s3_bucket.private_bucket.bucket}"" >> /home/ec2-user/webapp/.env
-echo "AWS_REGION="${var.region}"" >> /home/ec2-user/webapp/.env
+# resource "aws_instance" "my_ec2_instance" {
+#   count                       = 1
+#   ami                         = var.ami_id
+#   instance_type               = "t2.micro"
+#   vpc_security_group_ids      = [aws_security_group.app_security_group.id]
+#   subnet_id                   = aws_subnet.public_subnet[0].id
+#   associate_public_ip_address = true
+#   iam_instance_profile        = aws_iam_instance_profile.instance_profile.name
+#   key_name                    = "ec2-instance"
+#   user_data                   = <<EOF
+# #!/bin/bash
+# echo "SQLUSER="${var.sql_user}"" >> /home/ec2-user/webapp/.env
+# echo "SQLPASSWORD="${var.sql_password}"" >> /home/ec2-user/webapp/.env
+# echo "DATABASENAME=csye6225" >> /home/ec2-user/webapp/.env
+# echo "SQLHOSTNAME="${aws_db_instance.rds_instance.address}"" >> /home/ec2-user/webapp/.env
+# echo "BUCKETNAME="${aws_s3_bucket.private_bucket.bucket}"" >> /home/ec2-user/webapp/.env
+# echo "AWS_REGION="${var.region}"" >> /home/ec2-user/webapp/.env
 
-cd /home/ec2-user/webapp/
-sudo npm install -g pm2
-sudo pm2 start index.js --name csye6225 --log ./csye6225.log
-sudo pm2 startup systemd
-sudo pm2 save
-sudo pm2 list
-EOF
+# cd /home/ec2-user/webapp/
+# sudo npm install -g pm2
+# sudo pm2 start index.js --name csye6225 --log ./csye6225.log
+# sudo pm2 startup systemd
+# sudo pm2 save
+# sudo pm2 list
+# EOF
 
-  root_block_device {
-    volume_type           = "gp2"
-    volume_size           = 50
-    delete_on_termination = true
-  }
-  ebs_block_device {
-    device_name           = "/dev/sdb"
-    volume_type           = "gp2"
-    volume_size           = 20
-    delete_on_termination = true
-  }
-  credit_specification {
-    cpu_credits = "standard"
-  }
-  disable_api_termination = true
-  tags = {
-    Name = "my-ec2-instance-${count.index}"
-  }
-}
+#   root_block_device {
+#     volume_type           = "gp2"
+#     volume_size           = 50
+#     delete_on_termination = true
+#   }
+#   ebs_block_device {
+#     device_name           = "/dev/sdb"
+#     volume_type           = "gp2"
+#     volume_size           = 20
+#     delete_on_termination = true
+#   }
+#   credit_specification {
+#     cpu_credits = "standard"
+#   }
+#   disable_api_termination = true
+#   tags = {
+#     Name = "my-ec2-instance-${count.index}"
+#   }
+# }
--- loadbalancer.tf
+++ loadbalancer.tf
@@ -4,22 +4,56 @@ resource "aws_security_group" "loadbalancer" {
     name        = "loadbalancer"
     description = "Security group for the load balancer to access the web application"
     vpc_id      = aws_vpc.my_vpc.id
-
     ingress {
         from_port   = 80
         to_port     = 80
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
-
     ingress {
         from_port   = 443
         to_port     = 443
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
-        }
     }
-
+}
 output "loadbalancer_sg_id" {
     value = aws_security_group.loadbalancer.id
+}
+
+resource "aws_lb" "my_lb" {
+    name                        = "my-lb"
+    internal                    = false
+    load_balancer_type          = "application"
+    security_groups             = [aws_security_group.loadbalancer.id]
+    subnets                     = aws_subnet.public_subnet.*.id
+    enable_deletion_protection  = false
+    tags = {
+        Name = "my-lb"
+    }
+}
+
+resource "aws_lb_target_group" "my_target_group" {
+    name        = "my-target-group"
+    port        = "8080"
+    protocol    = "HTTP"
+    vpc_id      = aws_vpc.my_vpc.id
+    health_check {
+        interval                = 300
+        path                    = "/healthcheck"
+        protocol                = "HTTP"
+        timeout                 = 45
+        healthy_threshold       = 3
+        unhealthy_threshold     = 10
+    }
+}
+
+resource "aws_lb_listener" "lblistner" {
+    load_balancer_arn   = aws_lb.my_lb.arn
+    port                = "80"
+    protocol            = "HTTP"
+    default_action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.my_target_group.arn
+    }
 }
